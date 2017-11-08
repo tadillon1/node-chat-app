172b6125242d621d68f5fde67e746cf8d9b5f774
tadillon1 <tadillon@gmail.com>
2017-11-08 09:47:10 -0800

    Greet user user and alert others

--- server/server.js
+++ server/server.js
@@ -12,7 +12,21 @@ var io = socketIO(server);              //io is the web sockets server
 app.use(express.static(publicPath));    //Point express to the public folder containing index.html
 
 io.on('connection', (socket) => {     //event listener (listen for an event) in this case it is connection
-  console.log('New user connected');
+  // console.log('New user connected');
+
+
+  //Send message to newly joined user- Welcome to the chat app
+   socket.emit('newMessage', {
+     from: "Admin",
+     text: "Welcome to the chat app"
+   });
+  //broadast to all users except the newly joined user"New User joined" and createdAt
+  socket.broadcast.emit('newMessage', {
+     from: "Admin",
+     text: "New User Joined",
+     createdAt: new Date().getTime()
+   });
+
 
 
   socket.on('createMessage', (message) => {  // CUSTOM EVEN LISTENER for createMessage event
@@ -22,6 +36,11 @@ io.on('connection', (socket) => {     //event listener (listen for an event) in 
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', { //broadcast the message to everyone EXCEPT the user who sent it
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    //});
   });
 
   socket.on('disconnect', () => {   //event listener for disconnect.  I.E. the client drops off...
